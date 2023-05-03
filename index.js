cc93a6744a29ac75061b5895dbeca51738b336a2
masudra <masudrana6278312@gmail.com>
2023-05-03 20:26:49 +0600

    server created code

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors =require('cors')
 
 const data =require('./Data/Chef-recipi')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
